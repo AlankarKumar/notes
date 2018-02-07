beea056369d4b93b1bb91c779d5a0170b3d947c0
alankarkumar <kumar.alankar92@gmail.com>
2018-02-07 18:36:08 +0530

    node_modules

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-console.log('alankar');
-//this is text testing 
+console.log('starting app...');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+console.log('Result:', notes.add(9,-2));
+
+
